a3e8a9518ff955affe45d3e0108a9998806f0bc5
Emmanuel Opoku-Ameyaw <110876611+the-pro7@users.noreply.github.com>
2022-12-09 19:48:38 +0000

    updated today

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ $(document).ready(function() {
     $('button').prop('disabled', true)
     $('button').addClass('animted bounce');
 })
+
+// Gsap aimations
+const tl = gsap.timeline({defaults : {duration: 0.4}})
+tl.from('.attribution', {opacity: 0, y: 100, duration: 2})
+tl.from('figure', {opacity: 0, ease: 'bounce', duration: 0.5}, '<1')
+
