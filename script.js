78d471f9b6d1c4ec3466e4b77417bb12abe845b7
Emmanuel Opoku-Ameyaw <110876611+the-pro7@users.noreply.github.com>
2022-12-10 17:34:33 +0000

    updated version

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ $(document).ready(function() {
 // Gsap aimations
 const tl = gsap.timeline({defaults : {duration: 0.4}})
 tl.from('.attribution', {opacity: 0, y: 100, duration: 2})
-tl.from('figure', {opacity: 0, ease: 'bounce', duration: 0.5}, '<1')
+tl.fromTo('figure', {opacity: 0, ease: 'bounce', duration: 0.5}, {opacity: 1}, '<1')
 
